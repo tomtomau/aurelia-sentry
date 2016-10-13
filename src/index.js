1afc9dabae6da800bbd3d64d26247368d81d8a0f
justindra <justindra91@gmail.com>
2016-10-13 10:03:55 +1000

    Edited README and added some comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import { inject } from 'aurelia-framework';
 import { EventAggregator } from 'aurelia-event-aggregator';
 
+const USER_CONTEXT_EVENT = 'sentry:user-context:set';
+
 @inject(EventAggregator)
 export class SentryAppender {
   constructor(ea) {
     // @TODO: Maybe do something here to check for Raven?
+    // This check is required as sometimes 'ea' is undefined when the object is created.
     if (!ea) ea = new EventAggregator();
-    this._eventSubscription = ea.subscribe('sentry:user-context:set', (data) => {
+    this._eventSubscription = ea.subscribe(USER_CONTEXT_EVENT, (data) => {
       this.setUserContext(data);
     });
   }

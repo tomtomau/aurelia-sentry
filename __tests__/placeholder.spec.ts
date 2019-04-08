b8460abd8e9e2187740accdcccf2e8c2e55dbf70
Tom Newby <me@tomnewby.net>
2019-04-08 16:44:41 +1000

    chore(rm): remove tests

--- __tests__/placeholder.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('Placeholder test', () => {
-    it('Placeholder test', () => {
-        expect(1).toEqual(jasmine.any(Number));
-        expect('test').toEqual('test');
-        expect(false).toBeFalsy();
-    });
-});
